60e076655c299e972affcaafc036b226ecc61e09
badri2758 <68319027+badri2758@users.noreply.github.com>
2024-07-03 16:09:30 -0500

    Hobby Page added

---
+++ src/components/Hobby/Toolstack.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import { SiPostman, SiVercel,SiVisualstudiocode, SiVisualstudio, SiMicrosoftsqlserver, SiGitlab,SiGithub, SiWindows , SiSwagger, SiJenkins } from "react-icons/si";
+
+function Toolstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVisualstudio />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVisualstudiocode />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPostman />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiMicrosoftsqlserver />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVercel />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiWindows />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiGithub />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiGitlab />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiSwagger />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiJenkins />
+      </Col>
+      
+    </Row>
+  );
+}
+
+export default Toolstack;
